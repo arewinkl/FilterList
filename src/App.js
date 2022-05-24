9fb4829400b67a193f8c6cd2b26ee43baaba21e3
Alec Rewinkel <alecrewinkel@gmail.com>
2022-05-23 18:53:43 -0600

    style

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <div className="title-app">Filter List</div>
         <input className="input-app" />
         <button className="button-one-app">Submit</button>
       </header>
